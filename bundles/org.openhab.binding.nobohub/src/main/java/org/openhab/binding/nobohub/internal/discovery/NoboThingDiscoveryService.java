08079ad8a30ab16be1ed06a8bdb78b945b806d5b
Espen Fossen <espenaf@junta.no>
2021-02-15 15:12:22 +0100

    Improved discovery to only add devices not already added.

--- bundles/org.openhab.binding.nobohub/src/main/java/org/openhab/binding/nobohub/internal/discovery/NoboThingDiscoveryService.java
+++ bundles/org.openhab.binding.nobohub/src/main/java/org/openhab/binding/nobohub/internal/discovery/NoboThingDiscoveryService.java
@@ -1,22 +1,26 @@
 /**
  * Copyright (c) 2010-2020 Contributors to the openHAB project
- *
+ * <p>
  * See the NOTICE file(s) distributed with this work for additional
  * information.
- *
+ * <p>
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
- *
+ * <p>
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.binding.nobohub.internal.discovery;
 
-import static org.openhab.binding.nobohub.internal.NoboHubBindingConstants.*;
+import static org.openhab.binding.nobohub.internal.NoboHubBindingConstants.AUTODISCOVERED_THING_TYPES_UIDS;
+import static org.openhab.binding.nobohub.internal.NoboHubBindingConstants.THING_TYPE_COMPONENT;
+import static org.openhab.binding.nobohub.internal.NoboHubBindingConstants.THING_TYPE_ZONE;
+import static org.openhab.binding.nobohub.internal.NoboHubBindingConstants.VENDOR;
 
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -24,7 +28,7 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
 import org.eclipse.smarthome.config.discovery.DiscoveryResult;
 import org.eclipse.smarthome.config.discovery.DiscoveryResultBuilder;
-import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingUID;
 import org.openhab.binding.nobohub.internal.NoboHubBridgeHandler;
 import org.openhab.binding.nobohub.internal.model.Component;
@@ -66,55 +70,76 @@ public class NoboThingDiscoveryService extends AbstractDiscoveryService {
 
     public void detectZones(Collection<Zone> zones) {
         ThingUID bridge = bridgeHandler.getThing().getUID();
-        ThingTypeUID thingType = THING_TYPE_ZONE;
+        List<Thing> things = bridgeHandler.getThing().getThings();
 
         for (Zone zone : zones) {
-            ThingUID thingId = new ThingUID(thingType, bridge, Integer.toString(zone.getId()));
-            String label = zone.getName();
+            ThingUID discoveredThingId = new ThingUID(THING_TYPE_ZONE, bridge, Integer.toString(zone.getId()));
 
-            Map<String, Object> properties = new HashMap<>(1);
-            properties.put("id", Integer.toString(zone.getId()));
-            properties.put("name", zone.getName());
-            properties.put("vendor", VENDOR);
+            boolean addDiscoveredZone = true;
+            for (Thing thing : things) {
+                if (thing.getUID().equals(discoveredThingId)) {
+                    addDiscoveredZone = false;
+                }
+            }
 
-            logger.debug("Adding device {} to inbox", thingId);
-            DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingId).withBridge(bridge)
-                    .withLabel(label).withProperties(properties).withRepresentationProperty("id").build();
-            thingDiscovered(discoveryResult);
+            if (addDiscoveredZone) {
+                String label = zone.getName();
+
+                Map<String, Object> properties = new HashMap<>(1);
+                properties.put("id", Integer.toString(zone.getId()));
+                properties.put("name", zone.getName());
+                properties.put("vendor", VENDOR);
+
+                logger.debug("Adding device {} to inbox", discoveredThingId);
+                DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(discoveredThingId).withBridge(bridge)
+                        .withLabel(label).withProperties(properties).withRepresentationProperty("id").build();
+                thingDiscovered(discoveryResult);
+            }
         }
     }
 
     public void detectComponents(Collection<Component> components) {
         ThingUID bridge = bridgeHandler.getThing().getUID();
-        ThingTypeUID thingType = THING_TYPE_COMPONENT;
+        List<Thing> things = bridgeHandler.getThing().getThings();
 
         for (Component component : components) {
-            ThingUID thingId = new ThingUID(thingType, bridge, component.getSerialNumber().toString());
-            String label = component.getName();
+            ThingUID discoveredThingId = new ThingUID(THING_TYPE_COMPONENT, bridge,
+                    component.getSerialNumber().toString());
 
-            Map<String, Object> properties = new HashMap<>(1);
-            properties.put("serialNumber", component.getSerialNumber().toString());
-            properties.put("name", component.getName());
-            properties.put("vendor", VENDOR);
-            properties.put("model", component.getSerialNumber().getComponentType());
-
-            String zoneName = getZoneName(component.getZoneId());
-            if (zoneName != null) {
-                properties.put("zone", zoneName);
-            }
-
-            int zoneId = component.getTemperatureSensorForZoneId();
-            if (zoneId >= 0) {
-                String tempForZoneName = getZoneName(zoneId);
-                if (tempForZoneName != null) {
-                    properties.put("temperatureSensorForZone", tempForZoneName);
+            boolean addDiscoveredComponent = true;
+            for (Thing thing : things) {
+                if (thing.getUID().equals(discoveredThingId)) {
+                    addDiscoveredComponent = false;
                 }
             }
 
-            logger.debug("Adding device {} to inbox", thingId);
-            DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingId).withBridge(bridge)
-                    .withLabel(label).withProperties(properties).withRepresentationProperty("serialNumber").build();
-            thingDiscovered(discoveryResult);
+            if (addDiscoveredComponent) {
+                String label = component.getName();
+
+                Map<String, Object> properties = new HashMap<>(1);
+                properties.put("serialNumber", component.getSerialNumber().toString());
+                properties.put("name", component.getName());
+                properties.put("vendor", VENDOR);
+                properties.put("model", component.getSerialNumber().getComponentType());
+
+                String zoneName = getZoneName(component.getZoneId());
+                if (zoneName != null) {
+                    properties.put("zone", zoneName);
+                }
+
+                int zoneId = component.getTemperatureSensorForZoneId();
+                if (zoneId >= 0) {
+                    String tempForZoneName = getZoneName(zoneId);
+                    if (tempForZoneName != null) {
+                        properties.put("temperatureSensorForZone", tempForZoneName);
+                    }
+                }
+
+                logger.debug("Adding device {} to inbox", discoveredThingId);
+                DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(discoveredThingId).withBridge(bridge)
+                        .withLabel(label).withProperties(properties).withRepresentationProperty("serialNumber").build();
+                thingDiscovered(discoveryResult);
+            }
         }
     }
 
